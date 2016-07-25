899e4bc0dc33410a52aa931ef91f18caf69a849c
enniomara <enniomara1@gmail.com>
2016-07-26 00:52:25 +0200

    Separating server and frontend

--- gulp/tasks/build.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config').watch;
-
-gulp.task('build', ['vendor', 'vendor-styles', 'browserify', 'styles', 'html', 'browserSync-reload'], function() {
-  gulp.src(config.src);
-});
--- src/js/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Route} from 'react-router';
-import AppContainer from './components/AppContainer.jsx';
-import Home from './components/Home.jsx';
-import Login from './components/Login.jsx'
-
-export default (
-
-);
